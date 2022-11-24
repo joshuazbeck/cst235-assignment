36d22568b41608b57f2eab7fcf75fcf7190759b6
Josh Beck <82618412+joshuazbeck@users.noreply.github.com>
2022-11-23 22:01:40 -0600

    Adding security configuration

--- src/main/java/beans/User.java
+++ src/main/java/beans/User.java
@@ -1,7 +1,11 @@
 package beans;
 
+import java.security.Principal;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -37,5 +41,21 @@ public class User {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	
+	@PostConstruct 
+	public void init() {
+		// Get the logged in Principle
+		Principal principle= FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+			if(principle == null)
+			{
+				setFirstName("Unknown");
+				setLastName("");
+			}
+			else
+			{
+				setFirstName(principle.getName());
+				setLastName("");
+			}
 	}
 }
--- src/main/java/controllers/FormController.java
+++ src/main/java/controllers/FormController.java
@@ -1,11 +1,13 @@
 package controllers;
 
+import java.security.Principal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -35,31 +37,14 @@ public class FormController {
 	
 
 	//Code that is executed on form submit
-	public String onSubmit() {
-		
-		service.test();
-		
-		// Get the users from the input form
-		FacesContext context = FacesContext.getCurrentInstance();
-		User user = context.getApplication().evaluateExpressionGet(context, "#{user}", User.class);
-		
-		// Inject the user into the POST request
-		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
-		
-		Orders orders = new Orders();
-		// Inject the orders into the POST request
-		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("orders", service.getOrders());
+	public String onLogoff() {
 
-		
-		return "TestResponse.xhtml";
-	}
-	
-	//Code submitted to cause a redirect on press
-	public String onFlash(User user) {
-		// Inject the user into the POST request
-		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
+		// Invalidate the Session to clear the security token
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+			
+		// Redirect to a protected page (so we get a full HTTP Request) to get Login Page
+		return "TestResponse.xhtml?faces-redirect=true";
 
-		return "TestResponse2.xhtml?faces-redirect=true";
 	}
-	
+
 }
